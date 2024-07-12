8be2278eb00684eeb175e854f1ec6f926f88bae6
antoniostassi <stassiant@outlook.it>
2024-07-12 21:13:31 +0200

    HTML & JS | Some HTML Styling + Ticket card's script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 
 const calcButton = document.querySelector("form > div:last-child > button:first-child");
 let usrNameSurname; let usrKm; let usrAge; let finalPrice;
+let ticketCard = document.querySelector(".d-none");
 
 calcButton.addEventListener("click", (e) => {
+
     e.preventDefault();
     let usrInputName = document.getElementById("usr_name_lastname").value;
     let usrInputKM = document.getElementById("usr_km").value;
@@ -19,15 +21,28 @@ calcButton.addEventListener("click", (e) => {
         // Stampo i risultati delle variabili
         console.log("usrNameSurname: " + usrNameSurname + " | usrKm: " + usrKm + " | usrAge: " + usrAge);
 
-        finalPrice = (usrKm * 0.21).toFixed(2);
+        // Stabilisco il prezzo: 21 Centesimi al Km.
+        finalPrice = (usrKm * 0.21);
+
 
         if (usrAge == "Minorenne") {
-            finalPrice *= 0.8.toFixed(2);
+
+            // Se l'utente è Minorenne, pagherà il 20% in meno.
+            finalPrice *= 0.8;
+
         } else if (usrAge == "Over 65") {
-            finalPrice *= 0.6.toFixed(2);
+
+            // Se l'utente è Over 65, pagherà il 40% in meno.
+            finalPrice *= 0.6;
+
         }
 
         console.log(finalPrice);
+
+        ticketCard.classList.remove("d-none");
+        document.getElementById("passeggero").innerHTML = usrNameSurname;
+        document.getElementById("tariffa").innerHTML = usrAge;
+        document.getElementById("costo").innerHTML = finalPrice.toFixed(2);
     }
 
 });
